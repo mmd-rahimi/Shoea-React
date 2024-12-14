c933a3b216fd2d893b07f481d2db5a417f57b5b0
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-14 09:59:57 +0330

    ob1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { OnBoarding1 } from "./Components/onBoardingPages/OnBoarding1";
+
 export function App() {
     return(
-        <div className="w-[450px] h-[600px]">
-            
+        <div className="w-[450px] h-[600px] flex justify-center items-center">
+            <OnBoarding1/>
 
         </div>
     )
--- src/Components/onBoardingPages/OnBoarding1.tsx
+++ src/Components/onBoardingPages/OnBoarding1.tsx
@@ -1,3 +1,14 @@
-export function OnBoarding1 () {
-    
+import { GridLoader } from "react-spinners";
+
+export function OnBoarding1() {
+  return (
+    <div className="flex flex-col justify-between items-center gap-40 mt-32">
+      <div>
+        <img src="./public/shoea icon ob1.png" alt="" />
+      </div>
+      <div>
+        <GridLoader color="#000000" size={10} speedMultiplier={0.6} />
+      </div>
+    </div>
+  );
 }
