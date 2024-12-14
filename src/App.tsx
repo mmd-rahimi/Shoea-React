50f74bc7149afc9fdd55ea077dd5acf2ab0af7f6
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-14 10:11:29 +0330

    ob2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { OnBoarding1 } from "./Components/onBoardingPages/OnBoarding1";
+import { OnBoarding2 } from "./Components/onBoardingPages/OnBoarding2";
 
 export function App() {
     return(
         <div className="w-[450px] h-[600px] flex justify-center items-center">
-            <OnBoarding1/>
+            {/* <OnBoarding1/> */}
+            <OnBoarding2/>
 
         </div>
     )
---
+++ src/Components/onBoardingPages/OnBoarding2.tsx
@@ -0,0 +1,7 @@
+export function OnBoarding2 () {
+    return(
+        <div className="">
+            <img className="w-screen h-screen" src="./public/onboarding page 2.png" alt="" />
+        </div>
+    )
+}
