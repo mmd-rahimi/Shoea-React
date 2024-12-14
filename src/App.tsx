eda71e4596900b51ef7aaaac1ec1fdd66fe823f0
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-14 09:48:03 +0330

    some changes

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+    return(
+        <div className="w-[450px] h-[600px]">
+            
+
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,12 @@
+import { StrictMode } from 'react';
 import './index.css'
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+<StrictMode>
+    <App />
+</StrictMode>
+);
