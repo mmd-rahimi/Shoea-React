8bdcfa14a8abe1aeb9fa93e5b804ec79f2d8dc18
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-15 14:58:38 -0800

    router downloaded

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { OnBoarding2 } from "./Components/onBoardingPages/OnBoarding2";
 export function App() {
     return(
         <div className="w-[450px] h-[600px] flex justify-center items-center">
+            
             {/* <OnBoarding1/> */}
             <OnBoarding2/>
 
