cb3eb7a7bf708418d8dcca5e3e2b0af34e9815c7
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-16 13:54:39 -0800

    working on router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { OnBoarding1 } from "./Components/onBoardingPages/OnBoarding1";
 import { OnBoarding2 } from "./Components/onBoardingPages/OnBoarding2";
 
 export function App() {
     return(
         <div className="w-[450px] h-[600px] flex justify-center items-center">
-            
-            {/* <OnBoarding1/> */}
-            <OnBoarding2/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<OnBoarding1 />}>
+          {/* <Route index element={<Home />} /> */}
+          <Route path="/ob2" element={<OnBoarding2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
         </div>
     )
