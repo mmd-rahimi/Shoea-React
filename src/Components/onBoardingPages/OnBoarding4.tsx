5f027fe1f013e127a5c2e25795d3a3d508e61a3a
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-18 08:35:51 +0330

    added onboardings but not completed

---
+++ src/Components/onBoardingPages/OnBoarding4.tsx
@@ -0,0 +1,11 @@
+
+export function OnBoarding4() {
+    return(
+        <div className="h-screen flex flex-col items-center gap-6">
+            <img className="h-[60%] w-screen" src="./public/p4-bg.jfif" alt="" />
+            <p className="font-bold text-xl text-center">Your satisfaction is our <br /> number one periority</p>
+            <img className="mt-4" src="./public/swiper ob4.png" alt="" />
+            <button className="bg-slate-800 text-white rounded-2xl w-[23rem] h-8">Next</button>
+        </div>
+    )
+}
---
+++ src/Components/onBoardingPages/OnBoarding5.tsx
@@ -0,0 +1,11 @@
+
+export function OnBoarding4() {
+    return(
+        <div className="h-screen flex flex-col items-center gap-6">
+            <img className="h-[60%] w-screen" src="./public/p5-bg.png" alt="" />
+            <p className="font-bold text-xl text-center">Let’s fulfill your fashion <br /> needs with shoearight now!</p>
+            <img className="mt-4" src="./public/swiper ob5.png" alt="" />
+            <button className="bg-slate-800 text-white rounded-2xl w-[23rem] h-8">Get Started</button>
+        </div>
+    )
+}
