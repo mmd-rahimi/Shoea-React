1baa07c8598dcf8cffdbc176bdc3b06063d859d0
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-18 10:48:13 +0330

    header complete

--- src/Components/Home-Components/Header.tsx
+++ src/Components/Home-Components/Header.tsx
@@ -1,12 +1,16 @@
+import { FaRegBell, FaRegHeart } from "react-icons/fa";
 
 export function Header () {
     return(
         <div className="flex justify-between">
             <div className="flex gap-3">
-                <div><img src="" alt="" /></div>
-                <div>Good Morning 👋 <br /> mmd rahimi</div>
+                <div><img src="./public/profile img.png" alt="" /></div>
+                <div>Good Morning 👋 <br /><br /> mmd rahimi</div>
             </div>
-            <div className="flex gap-3"></div>
+            <div className="flex gap-3">
+                <div><FaRegBell /></div>
+                <div><FaRegHeart /></div>
+            </div>
         </div>
     )
 }
