ec98c49b2db970fa5d0cc0e39d4682988382d425
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-18 11:34:25 +0330

    navigate & router totaly completed

--- src/Components/onBoardingPages/OnBoarding5.tsx
+++ src/Components/onBoardingPages/OnBoarding5.tsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
 
 export function OnBoarding5() {
-    return(
-        <div className="h-screen flex flex-col items-center gap-6">
-            <img className="h-[60%] w-screen" src="./public/p5-bg.png" alt="" />
-            <p className="font-bold text-xl text-center">Let’s fulfill your fashion <br /> needs with shoearight now!</p>
-            <img className="mt-4" src="./public/swiper ob5.png" alt="" />
-            <button className="bg-slate-800 text-white rounded-2xl w-[23rem] h-8">Get Started</button>
-        </div>
-    )
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/Home");
+  };
+  return (
+    <div className="h-screen flex flex-col items-center gap-6">
+      <img className="h-[60%] w-screen" src="./public/p5-bg.png" alt="" />
+      <p className="font-bold text-xl text-center">
+        Let’s fulfill your fashion <br /> needs with shoearight now!
+      </p>
+      <img className="mt-4" src="./public/swiper ob5.png" alt="" />
+      <button
+        className="bg-slate-800 text-white rounded-2xl w-[23rem] h-8"
+        onClick={handleClick}
+      >
+        Get Started
+      </button>
+    </div>
+  );
 }
