a1a6f1b0c4568b143f5d15c0ea36f47fc194e8de
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-18 10:44:23 +0330

    starting home page

---
+++ src/Components/Home-Components/Header.tsx
@@ -0,0 +1,12 @@
+
+export function Header () {
+    return(
+        <div className="flex justify-between">
+            <div className="flex gap-3">
+                <div><img src="" alt="" /></div>
+                <div>Good Morning 👋 <br /> mmd rahimi</div>
+            </div>
+            <div className="flex gap-3"></div>
+        </div>
+    )
+}
